b1a54e5397add8090886bc9fd5c273851f07490d
Alessandro Costantini <alessandro.costantini@cnaf.infn.it>
2024-07-02 09:32:16 +0200

    Create oidc-blist.py

---
+++ oidc-blist.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import boto3
+import os
+
+OIDC_ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+#print(OIDC_ACCESS_TOKEN)
+
+sts_client = boto3.client('sts',
+    endpoint_url="https://<RGW_HOSTNAME>",
+    region_name=''
+    )
+
+response = sts_client.assume_role_with_web_identity(
+    RoleArn="arn:aws:iam:::role/IAMaccess",
+    RoleSessionName="test1",
+    DurationSeconds=3600,
+    WebIdentityToken=OIDC_ACCESS_TOKEN )
+
+s3client = boto3.client('s3',
+    aws_access_key_id = response['Credentials']['AccessKeyId'],
+    aws_secret_access_key = response['Credentials']['SecretAccessKey'],
+    aws_session_token = response['Credentials']['SessionToken'],
+    endpoint_url="https://<RGW_HOSTNAME>",
+    region_name='default',)
+
+resp = s3client.list_buckets()
+print(resp)
